e110d208ac38678625ee0a84e6c6bdd616f959ff
ytc1147819590 <34883875+ytc1147819590@users.noreply.github.com>
2018-09-07 10:49:15 +0800

    a

---
+++ es6/const.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2018/9/7.
+ */
+
+//PI 常量
+
+//const PI = 3.1415;
+//PI // 3.1415
+
+//const foo;
+// SyntaxError: Missing initializer in const declaration
+
+//对于const来说，只声明不赋值，就会报错。
+
+
+
---
+++ es6/for01es6.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2018/9/7.
+ */
+
+
+//var a = [];
+//for (var i = 0; i < 10; i++) {
+//    a[i] = function () {
+//        console.log(i);
+//    };
+//}
+//a[6](); // 10
+
+
+//var a = [];
+//for (let i = 0; i < 10; i++) {
+//    a[i] = function () {
+//        console.log(i);
+//    };
+//}
+//a[6](); // 6
+
+
+//for (let i = 0; i < 3; i++) {
+//    let i = 'abc';
+//    console.log(i);
+//}
+
+
+//let在for循环中每次都是不同的值，都有单独的作用域
