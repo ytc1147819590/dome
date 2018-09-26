b0803e473e643dbc2a6faecd3277feb299b09bf5
ytc1147819590 <34883875+ytc1147819590@users.noreply.github.com>
2018-09-26 15:56:12 +0800

    a

---
+++ es6/day02.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2018/9/26.
+ */
+
+
+//赋值
+//let [a,b,c]=[1,2,3];
+//
+//console.log(a,b,c);
+
+// 报错
+//let [foo] = 1;
+//let [foo] = false;
+//let [foo] = NaN;
+//let [foo] = undefined;
+//let [foo] = null;
+//let [foo] = {};
+
+//如果等号的右边不是数组（或者严格地说，不是可遍历的结构），那么将会报错。
---
+++ es6/day03.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2018/9/26.
+ */
+
+//function log(x , y){
+//    y = y || 'World';
+//    console.log(x , y);
+//}
+//log("hellw");
+//log("hellw","");
+//log("hellw","ytc");
+
+
+//function m1({x = 0, y = 0} = {}) {
+//    return [x, y];
+//}
+//console.log(m1({x:6,y:7}))
+
